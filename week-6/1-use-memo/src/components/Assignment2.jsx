ada39ad176e27bfeca009e86d0ecc1f0f66d1ede
omdharmik24@gmail.com <omdharmik24@gmail.com>
2024-01-10 19:02:33 +0530

    assignment2

--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -8,6 +8,7 @@ const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
 const TOTAL_LINES = 1000;
 const ALL_WORDS = [];
 for (let i = 0; i < TOTAL_LINES; i++) {
+    console.log("render")
     let sentence = "";
     for (let j = 0; j < words.length; j++) {
         sentence += (words[Math.floor(words.length * Math.random())])
