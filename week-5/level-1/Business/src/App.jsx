9b08573fb091c3c4d8eb0668c68d9d1a5994b9d9
omdharmik24@gmail.com <omdharmik24@gmail.com>
2024-01-10 15:13:09 +0530

    added css

---
+++ week-5/level-1/Business/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function BusinessCard(props){
+  return <>
+  <div className='card'>
+    <h1>{props.name}</h1>
+    <p>{props.description}</p>
+    <h3>Interests</h3>
+    <ul>
+      {props.interests.map((str,index) => <li key={index} style={{
+        listStyle: 'none'
+      }}>{str}</li>)}
+    </ul>
+    <button>Linkedin</button>
+    <button>Twitter</button>
+  </div>
+
+  </>
+
+}
+
+function App() {
+  return <>
+    <BusinessCard name="Om" description="learning fullstack" interests = {["coding", "reading", "travelling"]} />
+  </>
+}
+
+
+
+
+export default App
